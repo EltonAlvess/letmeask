632de61d58269494242e5dd6c0eeb171d7e4b980
Elton Alves <eltonosd@hotmail.com>
2021-07-19 21:18:32 -0300

    WIP - newRoom dev

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,11 +1,19 @@
+import { FormEvent, useState } from "react";
 import { Link } from "react-router-dom";
+import { Button } from "../components/Button";
+
 import illustrationImg from "../assets/images/illustration.svg";
 import logoImg from "../assets/images/logo.svg";
 import "../styles/auth.scss";
-import { Button } from "../components/Button";
 
 export function NewRoom() {
   //  const { user } = useAuth();
+  const [newRoom, setNewRoom] = useState('');
+
+async function handleCreateRoom(event: FormEvent){
+  event.preventDefault();
+  console.log(newRoom);
+}
 
   return (
     <div id="page-auth">
@@ -21,8 +29,13 @@ export function NewRoom() {
         <div className="main-content">
           <img src={logoImg} alt="Letmeask" />
           <h2>Create a new Room</h2>
-          <form action="">
-            <input type="text" placeholder="Enter with the room name" />
+          <form action="" onSubmit={handleCreateRoom}>
+            <input 
+              type="text" 
+              placeholder="Enter with the room name" 
+              onChange={event => setNewRoom(event.target.value)}
+              value={newRoom}
+              />
             <Button type="submit">Create Room</Button>
           </form>
           <p>
